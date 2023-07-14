722e31077d1b1a8e79afff7a9f1c061e3d8c842d
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-07-14 20:07:54 +0530

    Enable minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro"
             )
